725456a63afaf76cda24adb740e9497a583ba77c
bichanna <nobu.bichanna@gmail.com>
2022-02-15 20:04:57 -0600

    Add another error func

--- src/error.nim
+++ src/error.nim
@@ -1,4 +1,12 @@
+#
+# error.nim
+# SLAP
+#
+# Created by Nobuharu Shimazu on 2/15/2022
+# 
+
 import std/strformat, strutils
+import token
 
 type
   Error* = object
@@ -7,3 +15,6 @@ type
 proc error*(e: Error, line: int, errorName: string, message: string) =
   echo(fmt"line {line+1} -> {splitLines(e.source)[line]}")
   quit(fmt"{errorName}: {message}")
+
+proc error*(e: Error, token: Token, errorName: string, message: string) =
+  e.error(token.line, errorName, message)
