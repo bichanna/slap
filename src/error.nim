b52502da5577f9d008eaf83d6fb7c4591f34bb20
bichanna <nobu.bichanna@gmail.com>
2022-02-16 17:16:19 -0600

    Modify Error object

--- src/error.nim
+++ src/error.nim
@@ -10,7 +10,7 @@ import token
 
 type
   Error* = object
-    source: string
+    source*: string
 
 proc error*(e: Error, line: int, errorName: string, message: string) =
   echo(fmt"line {line+1} -> {splitLines(e.source)[line]}")
