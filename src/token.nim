94e21b631175a6b24a13dc68e85e245c9c253912
bichanna <nobu.bichanna@gmail.com>
2022-02-15 20:04:38 -0600

    Create token file

---
+++ src/token.nim
@@ -0,0 +1,75 @@
+#
+# token.nim
+# SLAP
+#
+# Created by Nobuharu Shimazu on 2/15/2022
+# 
+
+import std/strformat, strutils
+
+type
+  TokenType* = enum
+    Int                 = "INT"
+    Float               = "FLOAT"
+    String              = "STRING"
+    Identifier          = "IDENTIFIER"
+    LeftParen           = "LEFT_PAREN" # (
+    RightParen          = "RIGHT_PAREN" # )
+    LeftBrace           = "LEFT_BRACE" # [
+    RightBrace          = "RIGHT_BRACE" # ]
+    LeftBracket         = "LEFT_BRACKET" # {
+    RightBracket        = "RIGHT_BRACKET" # }
+    Colon               = "COLON" # :
+    SemiColon           = "SEMICOLON" # ;
+    Plus                = "PLUS" # +
+    Minus               = "MINUS" # -
+    Tilde               = "TILDE" # ~
+    Star                = "STAR" # *
+    Slash               = "SLASH" # /
+    Pound               = "POUND" # #
+    At                  = "AT" # @
+    Caret               = "CARET" # ^
+    Comma               = "COMMA" # ,
+    Bang                = "BANG" # !
+    LeftArrow           = "LEFT_ARROW" # <-
+    RightArrow          = "RIGHT_ARROW" # ->
+    Equals              = "EQUALS" # =
+    EqualEqual          = "EQUAL_EQUAL" # ==
+    GreaterEqual        = "GREATER_THAN_OR_EQUAL" # >=
+    Greater             = "GREATER_THAN" # >
+    LessEqual           = "LESS_THAN_OR_EQUAL" # <=
+    Less                = "LESS_THAN" # <
+    BangEqual           = "BANG_EQUAL" # !=
+    NewLine             = "NEW_LINE" # \n
+    DoubleDot           = "DOUBLE_DOT" # .
+    Dot                 = "DOT" # .
+    EOF                 = "EOF"
+
+    # keywords
+    Defun               = "defun"
+    Class               = "class"
+    And                 = "and"
+    Let                 = "let"
+    Const               = "const"
+    Or                  = "or"
+    If                  = "if"
+    Elif                = "elif" # this may be deleted in the future
+    Else                = "else"
+    For                 = "for"
+    Super               = "super"
+    While               = "while"
+    Self                = "self"
+    Return              = "return"
+    Continue            = "continue"
+    Break               = "break"
+    True                = "true"
+    False               = "false"
+    Null                = "null"
+
+  Token* = object
+    kind*: TokenType
+    value*: string
+    line*: int
+
+proc `$`*(token: Token): string =
+  return fmt"{token.kind}:{token.value}:{token.line}"
