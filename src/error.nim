03dd1407a0c3b6172414fd12158590b8b7c95002
bichanna <nobu.bichanna@gmail.com>
2022-02-15 20:00:11 -0600

    Remove the comments

--- src/error.nim
+++ src/error.nim
@@ -1,27 +1,3 @@
-# import strformat
-
-# import token
-# import tokenType
-
-# var hadError* = false
-
-# # Forward Declaration
-# proc report(line: int, where: string, message: string)
-
-# # Proc
-# proc error*(line: int, message: string) =
-#   report(line, "", message)
-
-# proc error*(token: Token, message: string) =
-#   if token.tkType == EOF:
-#     report(token.line, " at end", message)
-#   else:
-#     report(token.line, " at '" & token.lexeme & "'", message)
-
-# proc report(line: int, where: string, message: string) =
-#   echo &"[line {$line}] Error{$where}: {$message}"
-#   hadError = true
-
 import std/strformat, strutils
 
 type
