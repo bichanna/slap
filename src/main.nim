605a5ab7d826e432bcdeff9438661401fa1513c9
bichanna <nobu.bichanna@gmail.com>
2022-02-15 19:44:04 -0600

    Create the base of main file

---
+++ src/main.nim
@@ -0,0 +1,38 @@
+#
+# main.nim
+# SLAP
+#
+# Created by Nobuharu Shimazu on 2/15/2022
+# 
+
+import os
+
+# actually executes a source code
+proc execute(source: string) = 
+  discard
+
+# reads a file and pass it to the execute func
+proc runFile(path: string) =
+  execute(readFile(path))
+
+# starts a new REPL session
+proc repl() =
+  while true:
+    stdout.write(">> ")
+    execute(readline(stdin))
+
+# handles Ctrl-C
+proc handleCtrlC() {.noconv.} =
+  quit("\nBye!")
+
+when isMainModule:
+  # just to handle Ctrl-C
+  setControlCHook(handleCtrlC)
+
+  let params = commandLineParams()
+  if params.len > 1:
+    quit("USAGE: slap <file>.slp")
+  elif params.len == 1:
+    runFile(params[0])
+  else:
+    repl()
