44d976715786670deb4b553ef791554ac6bae88d
bichanna <nobu.bichanna@gmail.com>
2022-02-21 18:16:07 -0600

    Modify benchmark files

--- benchmark/fib/python3.py
+++ benchmark/fib/python3.py
@@ -2,5 +2,5 @@ def fib(n):
 	if n <= 1: return n
 	return fib(n - 2) + fib(n - 1)
 
-for i in range(0, 100):
+for i in range(0, 25):
 	fib(i)
---
+++ benchmark/prime/python3.py
@@ -0,0 +1,7 @@
+for num in range(2, 1500):
+	prime = True
+	for i in range(2, num):
+		if num % i == 0:
+			prime = False
+	if prime:
+		print(num)
