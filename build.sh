06e17c084d344a0c69d53df5b115c6ce595af2ac
bichanna <nobu.bichanna@gmail.com>
2022-02-21 23:06:03 -0600

    Update build file

--- build.sh
+++ build.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+LBLUE='\033[1;34m'
+NC='\033[0m'
+
 echo "Building SLAP..."
 nimble build --multimethods:on -d:release --silent
-echo "Completed"
-
-exit 0
+if [ $? -eq 0 ]; then
+	echo -e "${GREEN}Completed${NC}"
+	exit 0
+else
+	echo -e "${RED}Failed${NC}"
+	echo -e "Please run this command: ${LBLUE}nimble build --multimethods:on --verbose --debug${NC}"
+	echo "And open an issue on Github: https://github.com/bichanna/slap/issues"
+	exit 1
+fi 
