3f0dce6c573982080247c5e162cc6e0c3f14ffd7
bichanna <nobu.bichanna@gmail.com>
2022-02-25 17:27:55 -0600

    Add BreakException

--- src/exception.nim
+++ src/exception.nim
@@ -12,3 +12,7 @@ type
   # down to the body of the function
   ReturnException* = ref object of CatchableError
     value*: BaseType
+  
+  # just like ReturnException, this is used to unwind all the way
+  # down to the end of the for or while loop
+  BreakException* = ref object of CatchableError
