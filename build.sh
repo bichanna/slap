cf9ecf00200ee7529f7968fed5ed8b7657746a2f
bichanna <nobu.bichanna@gmail.com>
2022-02-25 18:10:09 -0600

    Fix indentation of build.sh

--- build.sh
+++ build.sh
@@ -28,16 +28,16 @@ nimble build --multimethods:on -d:release --silent
 
 # check if building the langauge finished with status 0 (success)
 if [ $? -eq 0 ]; then
-	cp ./main /usr/local/bin/slap
-	cp ./editor/slap.vim ~/.vim/syntax
-	echo "autocmd BufRead,BufNewFile *.slap set filetype=slap" >> ~/.vimrc
-	echo -e "${GREEN}Completed${NC}"
-	echo -e "${GREEN}Usage: 'slap [filename.slap]'${NC}"
-	exit 0
+  cp ./main /usr/local/bin/slap
+  cp ./editor/slap.vim ~/.vim/syntax
+  echo "autocmd BufRead,BufNewFile *.slap set filetype=slap" >> ~/.vimrc
+  echo -e "${GREEN}Completed${NC}"
+  echo -e "${GREEN}Usage: 'slap [filename.slap]'${NC}"
+  exit 0
 else
-	# there was a problem compiling the source code
-	echo -e "${RED}Failed${NC}"
-	echo -e "Please run this command: ${LBLUE}nimble build --multimethods:on --verbose --debug${NC}"
-	echo -e "And open an issue on Github: ${LBLUE}https://github.com/bichanna/slap/issues${NC}"
-	exit 1
+  # there was a problem compiling the source code of SLAP
+  echo -e "${RED}Failed${NC}"
+  echo -e "Please run this command: ${LBLUE}nimble build --multimethods:on --verbose --debug${NC}"
+  echo -e "And open an issue on Github: ${LBLUE}https://github.com/bichanna/slap/issues${NC}"
+  exit 1
 fi 
