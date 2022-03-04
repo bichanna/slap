052392a8ab88916e2f27c7332b8334fcc8b52db5
bichanna <nobu.bichanna@gmail.com>
2022-03-04 17:59:53 -0600

    Update main to show current SLAP version and help message

--- src/main.nim
+++ src/main.nim
@@ -5,7 +5,16 @@
 # Created by Nobuharu Shimazu on 2/15/2022
 # 
 
-import os, lexer, error, parser, interpreter, node, resolver
+import lexer, error, parser, interpreter, node, resolver
+import os, parseopt
+
+const HELP_MESSAGE = """
+usage: slap [--version] [--help] <filename>.slap
+-h, --help    : show this message and exit
+-v, --version : show current SLAP version and exit
+"""
+
+const CURRENT_VERSION = "0.0.1"
 
 # actually executes a source code
 proc execute(source: string) = 
@@ -44,6 +53,16 @@ proc repl() =
   except EOFError:
     quit("\nBye!")
 
+# show how to use SLAP CLI
+proc showHelp() =
+  stdout.write(HELP_MESSAGE & "\n")
+  quit(0)
+
+# show current SLAP version
+proc showVersion() =
+  stdout.write("SLAP " & CURRENT_VERSION & "\n")
+  quit(0)
+
 # handles Ctrl-C
 proc handleCtrlC() {.noconv.} =
   quit("\nBye!")
@@ -52,7 +71,17 @@ when isMainModule:
   # just to handle Ctrl-C
   setControlCHook(handleCtrlC)
 
-  let params = commandLineParams()
-  if params.len > 1: quit("Usage: slap [filename.slap]")
-  elif params.len == 1: runFile(params[0])
-  else: repl()
+  # handle command line arguments and options
+  var p = initOptParser(commandLineParams())
+  while true:
+    p.next()
+    case p.kind:
+    of cmdEnd: break
+    of cmdShortOption, cmdLongOption:
+      if p.key == "help" or p.key == "h":
+        showHelp()
+      elif p.key == "version" or p.key == "v":
+        showVersion()
+    of cmdArgument:
+      runFile(p.key)
+      break
