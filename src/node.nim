0fc2dee1e4b7a17643e80e49b1669f38ad3ae552
bichanna <nobu.bichanna@gmail.com>
2022-03-04 21:08:05 -0600

    Update nodes

--- src/node.nim
+++ src/node.nim
@@ -49,6 +49,7 @@ type
   ListOrMapVariableExpr* = ref object of Expr
     variable*: Expr
     indexOrKey*: Expr
+    token: Token
 
   AssignExpr* = ref object of Expr
     name*: Token
@@ -58,6 +59,7 @@ type
     variable*: Expr
     indexOrKey*: Expr
     value*: Expr
+    token: Token
 
   CallExpr* = ref object of Expr
     callee*: Expr
