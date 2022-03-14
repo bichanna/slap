39dd2e2488ba480fe2f70539e4255ecb6e8442a6
bichanna <nobu.bichanna@gmail.com>
2022-03-13 19:20:27 -0500

    Update env to store hashes instead of strings

--- src/env.nim
+++ src/env.nim
@@ -5,27 +5,49 @@
 # Created by Nobuharu Shimazu on 2/18/2022
 #
 
-import std/tables
+import tables, hashes
 import slaptype, token, error, objhash
 
 type
   # Environment holds all the variables and values
   Environment* = ref object
-    values*: Table[string, BaseType]
+    values*: Table[Hash, BaseType]
     enclosing*: Environment
+
+# forward declarations
+proc newEnv*(enclosing: Environment = nil): Environment
+proc define*(env: var Environment, name: string, value: BaseType)
+proc get*(env: var Environment, name: Token): BaseType
+proc ancestor*(env: var Environment, distance: int): Environment
+proc getAt*(env: var Environment, distance: int, name: string): BaseType
+proc assign*(env: var Environment, name: Token, value: BaseType)
+proc assignAt*(env: var Environment, distance: int, name: Token, value: BaseType)
+proc listOrMapAssign*(env: var Environment, name: Token, value: BaseType, indexOrKey: BaseType)
+proc listOrMapAssignAt*(env: var Environment, distance: int, name: Token, value: BaseType, indexOrKey: BaseType)
+
+# this awkward import is for recursive import; DO NOT REMOVE THIS!!
+import interpreterObj
+
+# --------------------------------------------------------------
 
 proc newEnv*(enclosing: Environment = nil): Environment =
   return Environment(
     enclosing: enclosing,
-    values: initTable[string, BaseType](),
+    values: initTable[Hash, BaseType](),
   )
 
 # binds a name to a value
-proc define*(env: var Environment, name: string, value: BaseType) = env.values[name] = value
+proc define*(env: var Environment, name: string, value: BaseType) =
+  var hashed: Hash
+  hashed = name.hash
+  env.values[hashed] = value
+
+# this is for import statements
+proc define*(env: var Environment, name: Hash, value: BaseType) = env.values[name] = value
 
 # looks up the variable and returns its value
 proc get*(env: var Environment, name: Token): BaseType =
-  if env.values.hasKey(name.value): return env.values[name.value]
+  if env.values.hasKey(name.value.hash): return env.values[name.value.hash]
   if not env.enclosing.isNil: return env.enclosing.get(name)
   else:
     error(name, "RuntimeError", "'" & name.value & "' is not defined")
@@ -35,23 +57,23 @@ proc ancestor*(env: var Environment, distance: int): Environment =
   for _ in 0 ..< distance: environment = environment.enclosing
   return environment
 
-proc getAt*(env: var Environment, distance: int, name: string): BaseType = return env.ancestor(distance).values[name]
+proc getAt*(env: var Environment, distance: int, name: string): BaseType = return env.ancestor(distance).values[name.hash]
 
 # assign
 proc assign*(env: var Environment, name: Token, value: BaseType) =
-  if env.values.hasKey(name.value):
-    env.values[name.value] = value
+  if env.values.hasKey(name.value.hash):
+    env.values[name.value.hash] = value
     return
   elif not env.enclosing.isNil:
     env.enclosing.assign(name, value)
     return
   error(name, "RuntimeError", "'" & name.value & "' is not defined")
 
-proc assignAt*(env: var Environment, distance: int, name: Token, value: BaseType) = env.ancestor(distance).values[name.value] = value
+proc assignAt*(env: var Environment, distance: int, name: Token, value: BaseType) = env.ancestor(distance).values[name.value.hash] = value
 
 proc listOrMapAssign*(env: var Environment, name: Token, value: BaseType, indexOrKey: BaseType) =
-  if env.values.hasKey(name.value):
-    let listOrMap = env.values[name.value]
+  if env.values.hasKey(name.value.hash):
+    let listOrMap = env.values[name.value.hash]
     if listOrMap of SlapList:
       if not (indexOrKey of SlapInt): error(name, "RuntimeError", "List indices must be integers")
       if SlapInt(indexOrKey).value < SlapList(listOrMap).values.len and SlapInt(indexOrKey).value > -1:
@@ -67,7 +89,7 @@ proc listOrMapAssign*(env: var Environment, name: Token, value: BaseType, indexO
     error(name, "RuntimeError", "'" & name.value & "' is not defined")
 
 proc listOrMapAssignAt*(env: var Environment, distance: int, name: Token, value: BaseType, indexOrKey: BaseType) =
-  let listOrMap = env.ancestor(distance).values[name.value]
+  let listOrMap = env.ancestor(distance).values[name.value.hash]
   if listOrMap of SlapList:
     if not (indexOrKey of SlapInt): error(name, "RuntimeError", "List indices must be integers")
     SlapList(listOrMap).values[SlapInt(indexOrKey).value] = value
