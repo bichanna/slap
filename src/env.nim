a6bade1cd19af4776781235a36b53d5a1b24ee66
bichanna <nobu.bichanna@gmail.com>
2022-03-23 15:55:18 -0500

    Add comments to env.nim

--- src/env.nim
+++ src/env.nim
@@ -25,41 +25,46 @@ proc assignAt*(env: var Environment, distance: int, name: Token, value: BaseType
 proc listOrMapAssign*(env: var Environment, name: Token, value: BaseType, indexOrKey: BaseType)
 proc listOrMapAssignAt*(env: var Environment, distance: int, name: Token, value: BaseType, indexOrKey: BaseType)
 
-# this awkward import is for recursive import; DO NOT REMOVE THIS!!
+# This awkward import is for recursive import; DO NOT REMOVE THIS!!
+# This is actually for debugging.
 # import interpreterObj
 
 # --------------------------------------------------------------
 
+# This proc creates a new Environment and returns it.
 proc newEnv*(enclosing: Environment = nil): Environment =
   return Environment(
     enclosing: enclosing,
     values: initTable[Hash, BaseType](),
   )
 
-# binds a name to a value
+# This binds a variable name to a SLAP value.
 proc define*(env: var Environment, name: string, value: BaseType) =
   var hashed: Hash
   hashed = name.hash
   env.values[hashed] = value
 
-# this is for import statements
+# This is for import statements.
 proc define*(env: var Environment, name: Hash, value: BaseType) = env.values[name] = value
 
-# looks up the variable and returns its value
+# This looks up the variable and returns its value.
+# If none is found, throws a RuntimeError.
 proc get*(env: var Environment, name: Token): BaseType =
   if env.values.hasKey(name.value.hash): return env.values[name.value.hash]
   if not env.enclosing.isNil: return env.enclosing.get(name)
   else:
     error(name, "RuntimeError", "'" & name.value & "' is not defined")
 
+# This proc returns the local environments with the distance of `distance`
 proc ancestor*(env: var Environment, distance: int): Environment =
   var environment = env
   for _ in 0 ..< distance: environment = environment.enclosing
   return environment
 
+# This proc uses the ancestor proc above to get local varibles
 proc getAt*(env: var Environment, distance: int, name: string): BaseType = return env.ancestor(distance).values[name.hash]
 
-# assign
+# This proc assigns a SLAP value to an already-existing varible.
 proc assign*(env: var Environment, name: Token, value: BaseType) =
   if env.values.hasKey(name.value.hash):
     env.values[name.value.hash] = value
@@ -69,8 +74,11 @@ proc assign*(env: var Environment, name: Token, value: BaseType) =
     return
   error(name, "RuntimeError", "'" & name.value & "' is not defined")
 
+# This proc uses the ancestor proc above to assign a SLAP value
+# to an already-existing variable.
 proc assignAt*(env: var Environment, distance: int, name: Token, value: BaseType) = env.ancestor(distance).values[name.value.hash] = value
 
+# This is essentially the same as the assign proc but used with map and lists.
 proc listOrMapAssign*(env: var Environment, name: Token, value: BaseType, indexOrKey: BaseType) =
   if env.values.hasKey(name.value.hash):
     let listOrMap = env.values[name.value.hash]
@@ -88,6 +96,7 @@ proc listOrMapAssign*(env: var Environment, name: Token, value: BaseType, indexO
   else:
     error(name, "RuntimeError", "'" & name.value & "' is not defined")
 
+# This is essentially the same as the assignAt proc but used with map and lists.
 proc listOrMapAssignAt*(env: var Environment, distance: int, name: Token, value: BaseType, indexOrKey: BaseType) =
   let listOrMap = env.ancestor(distance).values[name.value.hash]
   if listOrMap of SlapList:
