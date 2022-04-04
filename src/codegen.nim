9c4b0c74a7b5f721103eebb8995f9ac1a917a418
bichanna <nobu.bichanna@gmail.com>
2022-04-03 19:28:41 -0500

    Create TargetLang and CodeGenerator types

--- src/codegen.nim
+++ src/codegen.nim
@@ -5,6 +5,23 @@
 # Created by Nobuharu Shimazu on 4/2/2022
 #
 
+import node
+
 const
   StaticError = "StaticError"
-  
+
+type
+  # This enum specifies the target language which SLAP is compiled to.
+  # Currently, SLAP supports only JavaScript as a target language.
+  TargetLang* = enum
+    JAVASCRIPT,
+    # CPP,
+    # C,
+
+  # CodeGenerator takes in an abstract syntax tree and tanspile to
+  # the target language (currenlty, only JS is available).
+  CodeGenerator* = object
+    ast: seq[Stmt]
+    language: TargetLang
+
+
