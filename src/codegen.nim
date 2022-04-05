88caf844482bfde155c779e1cf2c9831bf4ad7a6
bichanna <nobu.bichanna@gmail.com>
2022-04-04 21:05:15 -0500

    Add methods for statement nodes except ImportStmt

--- src/codegen.nim
+++ src/codegen.nim
@@ -9,7 +9,9 @@ import node, token
 import sugar, sequtils, strutils
 
 const
-  StaticError = "StaticError"
+  ErrorName* = "CompilerError"
+
+var isNamedFunc = false
 
 type
   # This enum specifies the target language which SLAP is compiled to.
@@ -22,6 +24,9 @@ type
   CodeGenerator* = object
     ast: seq[Stmt]
     language: TargetLang
+
+proc newCodeGenerator*(nodes: seq[Stmt], language: TargetLang): CodeGenerator =
+  return CodeGenerator(ast: nodes, language: language)
 
 # These are the base methods for the emit methods below.
 method emit(self: CodeGenerator, expre: Expr): string {.base, locks: "unknown".} = discard
@@ -87,5 +92,50 @@ method emit(self: CodeGenerator, expre: FuncExpr): string =
     if arg of DefaultValued: return DefaultValued(arg).paramName.value & "=" & self.emit(DefaultValued(arg).default)
     if arg of RequiredArg: return RequiredArg(arg).paramName.value
     if arg of RestArg: return "..." & RestArg(arg).paramName.value
-  "(" & toSeq(0..<expre.parameters.len).map(i => getArgType(expre.parameters[i])).join(", ") & ") => " &
-    "{\n\t" & toSeq(0..<expre.body.len).map(i => self.emit(expre.body[i])).join("\n") & "}"
+  return "(" & toSeq(0..<expre.parameters.len).map(i => getArgType(expre.parameters[i])).join(", ") & ") " & (if isNamedFunc: "" else: "=> ") &
+    "{\n\t" & toSeq(0..<expre.body.len).map(i => self.emit(expre.body[i])).join("\n") & "\n}"
+
+# -----------------------------------------------------------------------------------------------
+
+method emit(self: CodeGenerator, statement: ExprStmt): string =
+  self.emit(statement.expression)
+
+method emit(self: CodeGenerator, statement: VariableStmt): string =
+  "var " & statement.name.value & " = " & self.emit(statement.init) & ";"
+
+method emit(self: CodeGenerator, statement: IfStmt): string =
+  result = "if (" & self.emit(statement.condition) & ") {\n" & self.emit(statement.thenBranch) & "\n}"
+  if statement.elifBranches.len != 0:
+    for each in statement.elifBranches:
+      result &= "else if (" & self.emit(each.condition) & ") {\n" & self.emit(each.thenBranch) & "\n}"
+  if not statement.elseBranch.isNil:
+    result &= "else {\n" & self.emit(statement.elseBranch) & "\n}"
+
+method emit(self: CodeGenerator, statement: BlockStmt): string =
+  toSeq(0..<statement.statements.len).map(i => self.emit(statement.statements[i])).join(";\n")
+
+method emit(self: CodeGenerator, statement: WhileStmt): string =
+  "while (" & self.emit(statement.condition) & ") " & self.emit(statement.body)
+
+method emit(self: CodeGenerator, statement: FuncStmt): string =
+  isNamedFunc = true
+  result = "function " & statement.name.value & self.emit(statement.function)
+  isNamedFunc = false
+
+method emit(self: CodeGenerator, statement: ReturnStmt): string =
+  "return " & (if statement.value.isNil: "" else: self.emit(statement.value)) & ";"
+
+method emit(self: CodeGenerator, statement: BreakStmt): string =
+  "break;"
+
+method emit(self: CodeGenerator, statement: ContinueStmt): string =
+  "continue;"
+
+# TODO: Implement emit method for ImportStmt node
+method emit(self: CodeGenerator, statement: ImportStmt): string =
+  discard
+
+method emit(self: CodeGenerator, statement: ClassStmt): string =
+  "class " & statement.name.value & (if statement.superclass.isNil: "" else: " extends " & self.emit(statement.superclass)) &
+    " {\n" & toSeq(0..<statement.methods.len).map(i => self.emit(statement.methods[i])).join(";\n") &
+    toSeq(0..<statement.classMethods.len).map(i => self.emit(statement.classMethods[i])).join(";\n") & "}"
