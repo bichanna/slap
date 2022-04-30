e3114193b2eca7329d219318ade59166e8b8de7e
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-04-30 10:36:56 -0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
-@REM I need help writing this; I know almost nothing about Windows!
+echo "stuff you'd want to type here." >> [filepath/name.filetype]
+# if this helps, this is a basic line of writing text into a file.
